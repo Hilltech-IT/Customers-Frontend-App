ce76c3c9103ef945f93d1c463f2ba3eb87cb89e8
Paul Ndambo <paulkadabo@gmail.com>
2025-07-13 17:44:06 +0300

    fixes

--- src/services/constants.jsx
+++ src/services/constants.jsx
@@ -1,4 +1,4 @@
-export const BACKEND_API_URL = "https://api.stayzhubprovider.com";
-export const PROPERTIES_API_URL = "https://api.stayzhubprovider.com/api/properties/api";
-export const BOOKINGS_API_URL = "https://api.stayzhubprovider.com/api/bookings/api";
-export const USERS_API_URL = "https://api.stayzhubprovider.com/api/users";
+export const BACKEND_API_URL = "https://api.stayzhubprovider.com" //"https://api.stayzhubprovider.com";
+export const PROPERTIES_API_URL = "https://api.stayzhubprovider.com" //"https://api.stayzhubprovider.com/api/properties/api";
+export const BOOKINGS_API_URL = "https://api.stayzhubprovider.com" //"https://api.stayzhubprovider.com/api/bookings/api";
+export const USERS_API_URL = "https://api.stayzhubprovider.com" //"https://api.stayzhubprovider.com/api/users";
